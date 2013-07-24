45dc93e2343af7c38c5da8cceb34c92cb069c649
Saketh Kasibatla <sake.kasi@gmail.com>
2013-07-23 23:10:23 -0700

    finished implementing feature complete website. need to add content

--- bootstrap/js/script.js
+++ bootstrap/js/script.js
@@ -1,9 +1,40 @@
+var content_height = '350px';
+var collapsed_height = '0px';
+
+function clicker (id){
+	return function(){
+		console.log(id + ' clicked');
+		$('.page-content').animate({ height: 'hide', opacity: 'hide' }, 'fast');//.hide();
+		$(id +'.page-content').animate({ height: 'show', opacity: 'show' }, 'fast');//.show();
+		$('.content-text').animate({
+		 		height: content_height
+		 	}, 500, function(){});
+	}
+};
+
 $(document).ready(function(){
 	$('.hidden').hide();
 
-	$('#about-me.menu-entry').click(function(){
-		console.log('about me clicked');
-		$('div#main-menu.page-content').hide();
-		$('div#about-me.page-content').show();
+	var ids = ['about-me', 'my-projects', 'work-experience', 'interests', 'contact'];
+
+	ids.map(function(item){
+		$('#'+item+'.menu-entry').click(clicker('#'+item));
+	});
+
+	ids.map(function(item){
+		$('#'+item+'.page-content .content-text').load("content/"+item+".html");
+	});
+
+	$('.back').click(function(){
+		console.log('back clicked');
+		$('.content-text').animate({
+		 		height: collapsed_height
+		 	}, 500, function(){});
+		setTimeout(function(){
+			$('.page-content').animate({ height: 'hide', opacity: 'hide' }, 'fast');//.hide();
+			$('#main-menu.page-content').animate({ height: 'show', opacity: 'show' }, 'fast');//.show();
+		}, 500);
 	});
 });
+
+

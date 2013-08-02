60754659044bf164a4a1a012f5d2f6ae5a1ffe95
Saketh Kasibatla <sake.kasi@gmail.com>
2013-08-01 18:38:52 -0700

    added more content and provided key bindings to navigate

--- bootstrap/js/script.js
+++ bootstrap/js/script.js
@@ -1,25 +1,39 @@
 var faster = 150;
+var ids = ['about-me', 'my-projects', 'work-experience', 'interests', 'contact'];
+var in_menu = true;
 
 function clicker (id){
 	return function(){
 		console.log(id + ' clicked');
 		$('.page-content').animate({ height: 'hide', opacity: 'hide' }, faster);//.hide();
 		$(id +'.page-content').animate({ height: 'show', opacity: 'show' }, 'fast');//.show();
+
+		in_menu = false;
+	}
+};
+
+function go_back (){
+	if (!in_menu){
+		$('.page-content').animate({ height: 'hide', opacity: 'hide' }, faster);//.hide();
+		$('#main-menu.page-content').animate({ height: 'show', opacity: 'show' }, 'fast');//.show();
+		in_menu = true;
 	}
 };
 
 $(document).ready(function(){
-	var ids = ['about-me', 'my-projects', 'work-experience', 'interests', 'contact'];
+	in_menu = true;
 
 	var tokens = $(location).attr('href').split("#");
 	var index = $.inArray(tokens.slice(-1)[0], ids);
 	if(  index != -1 ){
 		$('#'+ids[index]+'.page-content').removeClass('hidden');
 		$('#main-menu.page-content').addClass('hidden');
+
+		in_menu = false;
 	}
 
 	$('.hidden').hide();
-	
+
 
 	ids.map(function(item){
 		$('#'+item+'.menu-entry').click(clicker('#'+item));
@@ -28,13 +42,14 @@ $(document).ready(function(){
 	ids.map(function(item){
 		$('#'+item+'.page-content .content-text').load("content/"+item+".html");
 	});
-
 	
 
-	$('.back').click(function(){
-		$('.page-content').animate({ height: 'hide', opacity: 'hide' }, faster);//.hide();
-		$('#main-menu.page-content').animate({ height: 'show', opacity: 'show' }, 'fast');//.show();
-	});
+	$('.back').click(go_back);
+
+	$("body").keydown(function(e) {
+	  if(e.keyCode == 27) { // esc
+	    go_back();
+	    window.location = $('.back a').attr('href');
+	  }
+	});	
 });
-
-
